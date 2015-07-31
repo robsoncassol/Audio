12264becdacfe6eb198ccdabb9345cc921e4de0d
robsoncassol <robsoncassol@gmail.com>
2015-07-31 16:40:25 -0300

    main storyboard

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func play(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
